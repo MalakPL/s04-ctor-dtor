a3fe9bbcdf8d2b5923724ebd4c57bc6b4cce9e6c
Malak <szmajdakarol@gmail.com>
2021-11-08 01:44:17 +0100

    Dodaj pliki projektów.

---
+++ s04-ctor-dtor/s04-ctor-dtor.cpp
@@ -0,0 +1,48 @@
+﻿/*
+	Zrobiłem z obsługą znaków UTF-8
+*/
+
+#include <iostream>
+#include <io.h>
+#include <fcntl.h>
+
+using std::wcout;
+using std::wcerr;
+using std::endl;
+using std::wstring;
+using std::exception;
+
+struct test
+{
+	wstring text;
+
+	test(wstring text)
+	{
+		this->text = text;
+	}
+
+	~test()
+	{
+		wcout << "DESTRUCTION! " << text;
+	}
+};
+
+auto main() -> int
+{
+	try
+	{
+		if (_setmode(_fileno(stdout), _O_U8TEXT) == -1)
+		{
+			throw exception{ "Nie można ustawić kodowania UTF-8" };
+		}
+
+		test obiekt{ L"\\(ò_ó)/" };
+	}
+	catch (exception ex)
+	{
+		wcerr << "Wyjątek: " << ex.what() << endl;
+		return 1;
+	}
+
+	return 0;
+}
